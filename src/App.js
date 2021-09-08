346e980650dd2b467c6f8b632b05dfa858286867
mrogovets <mrogovets@ukr.net>
2021-09-08 23:00:10 +0300

    added cards & footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./materialize.css";
 import { NavPanel } from "./components/nav-panel/NavPanel";
 import { Slider } from "./components/slider/Slider";
+import { Footer } from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
       <NavPanel />
       <Slider />
+      <Footer />
     </div>
   );
 }
