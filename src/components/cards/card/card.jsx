ebbb6e2d4c11ac3c49cf23a7404d0283caa49674
mrogovets <mrogovets@ukr.net>
2021-09-12 22:40:03 +0300

    card 3elem in row -> img sach as props

--- src/components/cards/card/card.jsx
+++ src/components/cards/card/card.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./card.css";
 
-export const Card = () => {
+export const Card = ({ img }) => {
   return (
     <div className="col s12 m7">
       <div className="card horizontal">
         <div className="card-image">
-          <img src="https://lorempixel.com/100/190/nature/6" alt="img" />
+          <img src={img} alt="img" />
         </div>
         <div className="card-stacked">
           <div className="card-content">
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,13 +1,86 @@
 import React from "react";
 import { Card } from "./card/card";
 import "./cards.css";
+
+import img from "./pictures/icons8-бумажник-40.png";
 export const Cards = () => {
   return (
     <div className="cards-container">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <div className="cards-container-item">
+        <Card img={img} />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
+      <div className="cards-container-item">
+        <Card />
+      </div>
     </div>
   );
 };
