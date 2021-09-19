fbc147bdb4725ab0b26d53333920db9604269773
mrogovets <mrogovets@ukr.net>
2021-09-20 00:39:25 +0300

    adaptive for @media (max-width: 960px)

--- src/components/nav-panel/NavPanel.jsx
+++ src/components/nav-panel/NavPanel.jsx
@@ -24,30 +24,32 @@ export const NavPanel = () => {
           <a href="#!">three</a>
         </li>
       </ul>
-      <nav>
-        <div className="nav-wrapper">
+      <nav className="nav-main">
+        <div className="nav-logo">
           <a href="#!" className="brand-logo">
             <div className="logo-imgs">
               <img className="logo-img" src={LogoImg} alt="logo" />
-              <h5>Центр підготовки гуманітарного розмінування</h5>
+              <h5 className="logo-text">
+                Центр підготовки гуманітарного розмінування
+              </h5>
             </div>
           </a>
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Про Центр</a>
-            </li>
-            <li>
-              <a href="badges.html">З'язатися з нами</a>
-            </li>
-            {/* <!-- Dropdown Trigger --> */}
-            <li>
-              <a className="dropdown-trigger" href="#!" data-target="dropdown1">
-                Керівні документи
-                <i className="material-icons right">arrow_drop_down</i>
-              </a>
-            </li>
-          </ul>
         </div>
+        <ul className="right nav-menu">
+          <li>
+            <a href="#!">Про Центр</a>
+          </li>
+          <li>
+            <a href="#!">З'язатися з нами</a>
+          </li>
+          {/* <!-- Dropdown Trigger --> */}
+          <li>
+            <a className="dropdown-trigger" href="#!" data-target="dropdown1">
+              Керівні документи
+              <i className="material-icons right">arrow_drop_down</i>
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
